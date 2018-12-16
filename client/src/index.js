aa1667e7847059d0b7ce21560cc7519b214ed93c
zep1994 <snaxbags@protonmail.com>
2018-12-16 14:04:04 -0600

    Add Redux dependencies and react mdl

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { store } from './store';
+import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
